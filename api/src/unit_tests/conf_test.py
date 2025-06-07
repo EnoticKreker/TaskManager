5d42b793675141caeec0533588db0c5ba03db4ae
Dan Oborin Comp Wind <Daser.Eroker@yandex.ru>
2025-06-07 15:02:29 +0500

    up

--- api/src/unit_tests/conf_test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-@pytest.fixture(autouse=True)
-def set_test_env(monkeypatch):
-    monkeypatch.setenv("DB_HOST", "localhost")
-    monkeypatch.setenv("DB_PORT", "5432")
-    monkeypatch.setenv("DB_USER", "postgres")
-    monkeypatch.setenv("DB_PASS", "1")
-    monkeypatch.setenv("DB_NAME", "postgres")
